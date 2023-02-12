515de00dca5fe59e5393f6e2b6dd641af2867693
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-02-12 10:57:27 +0100

    Skip wrapped lines when moving with j/k
    I'm used to original vim behaviour where j/k movements navigate by
    physical lines rather than display lines.
    
    Furthermore, it breaks macros which include j/k and there are line wraps
    in the buffer.

--- init.lua
+++ init.lua
@@ -114,10 +114,6 @@ vim.g.maplocalleader = ' '
 -- Keymaps for better default experience
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
 -- [[ Highlight on yank ]]
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
