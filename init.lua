696545cec3f7ad172b3e20159bb5eff4d29f1546
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-02-12 12:19:32 +0100

    Create new horizontal split windows at bottom

--- init.lua
+++ init.lua
@@ -98,6 +98,7 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.ignorecase = true
 vim.o.mouse = 'a'
 vim.o.smartcase = true
+vim.o.splitbelow = true
 vim.o.termguicolors = true
 vim.o.undofile = true
 vim.o.updatetime = 250
