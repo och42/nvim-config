25709b18598173dd612cf98c35b255174c456f98
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-02-12 12:16:10 +0100

    Order vim options alphabetically

--- init.lua
+++ init.lua
@@ -92,18 +92,18 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 })
 
 -- [[ Setting options ]]
-vim.o.hlsearch = false
-vim.wo.number = true
-vim.o.mouse = 'a'
-vim.o.breakindent = true
-vim.o.undofile = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.updatetime = 250
-vim.wo.signcolumn = 'yes'
-vim.o.termguicolors = true
 vim.cmd([[colorscheme onedark]])
+vim.o.breakindent = true
 vim.o.completeopt = 'menuone,noselect'
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.mouse = 'a'
+vim.o.smartcase = true
+vim.o.termguicolors = true
+vim.o.undofile = true
+vim.o.updatetime = 250
+vim.wo.number = true
+vim.wo.signcolumn = 'yes'
 
 -- [[ Basic Keymaps ]]
 -- Set <Space> as the leader key
