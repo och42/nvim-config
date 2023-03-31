a598ffc24419a24dd78fcf4ea022f0adb26f71e7
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-03-31 09:03:59 +0200

    Show file relative path in status line

--- init.lua
+++ init.lua
@@ -190,6 +190,9 @@ require('lualine').setup({
     component_separators = '|',
     section_separators = '',
   },
+  sections = {
+    lualine_c = { { 'filename', path = 1 } },
+  },
 })
 
 -- Enable Comment.nvim
