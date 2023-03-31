2573a22c84f37ad9956d9a35e1db8e652230dabb
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-03-31 08:51:28 +0200

    Configure disable diagnostic keymap
    In some cases diagnostics are not helpful. For example when working with
    Helm charts which are YAML documents with Go templates. Since in general
    they are not valid YAML documents, LSP shows false errors which make
    code less readable. In such cases it is better to just turn the
    diagnostics off.

--- init.lua
+++ init.lua
@@ -349,6 +349,9 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>dd', function()
+  vim.diagnostic.disable(0)
+end, { desc = '[D]isable [D]iagnostics' })
 
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
