671db2b4763a5194114adafabd625a5b33afa32c
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-07-15 18:36:52 +0200

    Change keymaps for gitsigns chunks

--- init.lua
+++ init.lua
@@ -217,25 +217,8 @@ require('gitsigns').setup({
     end
 
     -- Navigation
-    map('n', ']c', function()
-      if vim.wo.diff then
-        return ']c'
-      end
-      vim.schedule(function()
-        gs.next_hunk()
-      end)
-      return '<Ignore>'
-    end, { expr = true })
-
-    map('n', '[c', function()
-      if vim.wo.diff then
-        return '[c'
-      end
-      vim.schedule(function()
-        gs.prev_hunk()
-      end)
-      return '<Ignore>'
-    end, { expr = true })
+    map('n', ']g', gs.next_hunk, { buffer = bufnr })
+    map('n', '[g', gs.prev_hunk, { buffer = bufnr })
 
     map('n', '<Leader>gb', function()
       gs.blame_line({ full = true })
