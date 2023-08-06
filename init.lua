fce22e6f01bc81bc3b47b5514034fb0cef0ec543
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-08-06 12:13:00 +0200

    Configure incremental scope selection keymaps

--- init.lua
+++ init.lua
@@ -67,6 +67,15 @@ require('lazy').setup({
         ensure_installed = { 'lua', 'vim', 'vimdoc' },
         highlight = { enable = true },
         indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<C-Space>',
+            node_decremental = '<M-Space>',
+            node_incremental = '<C-Space>',
+            scope_incremental = '<C-Space>',
+          },
+        },
       })
     end,
   },
