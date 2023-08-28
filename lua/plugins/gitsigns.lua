bd72d5156d3ed9a7325d609047504be8e31d13b6
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-08-28 19:28:58 +0200

    Refactor gitsigns configuration

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,10 +3,11 @@ return {
 
   opts = {
     on_attach = function(bufnr)
-      vim.keymap.set('n', '[h', require('gitsigns').prev_hunk)
-      vim.keymap.set('n', ']h', require('gitsigns').next_hunk)
+      local gitsigns = require('gitsigns')
+      vim.keymap.set('n', '[h', gitsigns.prev_hunk)
+      vim.keymap.set('n', ']h', gitsigns.next_hunk)
       vim.keymap.set('n', '<Leader>gb', function()
-        require('gitsigns').blame_line({ full = true })
+        gitsigns.blame_line({ full = true })
       end)
     end,
   },
