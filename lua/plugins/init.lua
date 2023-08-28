37da36e538a3f18f21a5c6d54e28d466e93f92a9
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-08-28 12:43:38 +0200

    Extract gitsigns configuration

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,18 +4,6 @@ return {
     config = true,
   },
   {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      on_attach = function(bufnr)
-        vim.keymap.set('n', '[h', require('gitsigns').prev_hunk, { buffer = bufnr, desc = 'Go to previous [H]unk' })
-        vim.keymap.set('n', ']h', require('gitsigns').next_hunk, { buffer = bufnr, desc = 'Go to next [H]unk' })
-        vim.keymap.set('n', '<Leader>gb', function()
-          require('gitsigns').blame_line({ full = true })
-        end, { buffer = bufnr, desc = '[G]it [B]lame line' })
-      end,
-    },
-  },
-  {
     'windwp/nvim-autopairs',
     config = true,
   },
