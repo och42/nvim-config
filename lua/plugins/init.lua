0021a74f2e26f2cf082f44b11067efb00df2a1c2
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-08-28 12:51:10 +0200

    Extract Treesitter configuration

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -35,26 +35,6 @@ return {
     version = '*',
   },
   {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = { 'lua', 'vim', 'vimdoc' },
-        highlight = { enable = true },
-        indent = { enable = true },
-        -- incremental_selection = {
-        --   enable = true,
-        --   keymaps = {
-        --     init_selection = '<C-Space>',
-        --     node_decremental = '<M-Space>',
-        --     node_incremental = '<C-Space>',
-        --     scope_incremental = '<C-Space>',
-        --   },
-        -- },
-      })
-    end,
-  },
-  {
     'navarasu/onedark.nvim',
     config = function()
       vim.cmd.colorscheme('onedark')
