7f9ba1d38e740790f2ca5f0b75cd6bb367f127e5
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-08-28 17:54:29 +0200

    Extract ensure lazy

--- init.lua
+++ init.lua
@@ -1,21 +1,9 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- [[ Basic Keymaps ]]
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+require('ensure-lazy')
 require('lazy').setup('plugins')
 
 -- [[ Setting options ]]
