14530dfd32562a0b9b562f0cbcd78497753ada93
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-08-28 18:48:38 +0200

    Delete keymaps descriptions

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,11 +3,11 @@ return {
 
   opts = {
     on_attach = function(bufnr)
-      vim.keymap.set('n', '[h', require('gitsigns').prev_hunk, { buffer = bufnr, desc = 'Go to previous [H]unk' })
-      vim.keymap.set('n', ']h', require('gitsigns').next_hunk, { buffer = bufnr, desc = 'Go to next [H]unk' })
+      vim.keymap.set('n', '[h', require('gitsigns').prev_hunk)
+      vim.keymap.set('n', ']h', require('gitsigns').next_hunk)
       vim.keymap.set('n', '<Leader>gb', function()
         require('gitsigns').blame_line({ full = true })
-      end, { buffer = bufnr, desc = '[G]it [B]lame line' })
+      end)
     end,
   },
 }
