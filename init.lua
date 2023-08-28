552dc5b1a054b81d15f25a2b0864f2bd620eb5ce
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-08-28 17:42:39 +0200

    Refactor highlight on yank

--- init.lua
+++ init.lua
@@ -44,13 +44,11 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 })
 
 -- [[ Highlight on yank ]]
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
-  pattern = '*',
+  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
 })
 
 -- Keymaps for better default experience
