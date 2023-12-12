0c03924d7b8f3b5936aadf5843aa5fd690f3ad4e
Jan Šidlo <1909821+och42@users.noreply.github.com>
2023-12-12 06:38:33 +0100

    Hide .git directory in netrw

--- lua/netrw.lua
+++ lua/netrw.lua
@@ -3,6 +3,6 @@ vim.g.netrw_liststyle = 3
 -- stylua: ignore
 vim.api.nvim_exec2([[
 
-let g:netrw_list_hide = netrw_gitignore#Hide()
+let g:netrw_list_hide = netrw_gitignore#Hide() .. ',.git/'
 
 ]], { output = false })
