117506df89f69ad8c113b8b7529f25fc60b3716e
Jan Šidlo <1909821+och42@users.noreply.github.com>
2024-01-13 17:29:02 +0100

    Change keymap for git blame

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,7 +6,7 @@ return {
       local gitsigns = require('gitsigns')
       vim.keymap.set('n', '[h', gitsigns.prev_hunk)
       vim.keymap.set('n', ']h', gitsigns.next_hunk)
-      vim.keymap.set('n', '<Leader>gb', function()
+      vim.keymap.set('n', '<M-g>b', function()
         gitsigns.blame_line({ full = true })
       end)
     end,
