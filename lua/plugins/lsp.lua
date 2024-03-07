cb8806d8c13f1cdaf27e7bc97f5ac084ef241d63
Jan Šidlo <1909821+och42@users.noreply.github.com>
2024-03-07 14:20:30 +0100

    Change keymaps for LSP actions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -36,10 +36,10 @@ return {
             capabilities = capabilities,
             settings = lsp_servers_configurations[server_name],
             on_attach = function(_, bufnr)
-              vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, { buffer = bufnr })
+              vim.keymap.set('n', '<M-c>a', vim.lsp.buf.code_action, { buffer = bufnr })
               vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
               vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { buffer = bufnr })
-              vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, { buffer = bufnr })
+              vim.keymap.set('n', '<M-r>n', vim.lsp.buf.rename, { buffer = bufnr })
             end,
           })
         end,
