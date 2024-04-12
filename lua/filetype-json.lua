97dc86955ad231757af439dce1109ac2efc1f971
Jan Šidlo <1909821+och42@users.noreply.github.com>
2024-04-12 08:09:29 +0200

    Set filetype json for devbox.lock files

--- lua/filetype-json.lua
+++ lua/filetype-json.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-  pattern = 'composer.lock,flake.lock',
+  pattern = 'composer.lock,devbox.lock,flake.lock',
   callback = function()
     vim.bo.filetype = 'json'
   end,
