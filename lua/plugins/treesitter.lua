3796a1fc6bc8e4d48624272a0a74e462259c45e6
Jan Šidlo <1909821+och42@users.noreply.github.com>
2024-04-14 11:35:56 +0200

    Fix nvim-treesitter configuration
    This change only resets default values because nvim-treesitter requires
    to set all fields. Otherwise, LSP shows warning.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,13 @@ return {
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup({
+      auto_install = false,
       ensure_installed = { 'lua', 'vim', 'vimdoc' },
       highlight = { enable = true },
+      ignore_install = {},
       indent = { enable = true },
+      modules = {},
+      sync_install = false,
     })
   end,
 }
