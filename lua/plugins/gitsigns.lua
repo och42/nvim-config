013f13219e2373011b8999406a2aed858c1a2556
Jan Šidlo <1909821+och42@users.noreply.github.com>
2024-04-17 15:45:24 +0200

    Update gitsigns keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,8 +6,11 @@ return {
       local gitsigns = require('gitsigns')
       vim.keymap.set('n', '[h', gitsigns.prev_hunk)
       vim.keymap.set('n', ']h', gitsigns.next_hunk)
-      vim.keymap.set('n', '<M-g>b', function()
+      vim.keymap.set('n', '\\gb', function()
         gitsigns.blame_line({ full = true })
+      end)
+      vim.keymap.set('n', '\\gB', function()
+        gitsigns.blame_line({ full = true, ignore_whitespace = true })
       end)
     end,
   },
