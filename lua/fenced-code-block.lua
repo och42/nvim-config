e0370e861d1e94507dde3ff6cf9b1b5b51b67cee
Jan Šidlo <1909821+och42@users.noreply.github.com>
2024-12-03 23:05:08 +0100

    Create insert fenced block functionality

---
+++ lua/fenced-code-block.lua
@@ -0,0 +1,8 @@
+local function insert_fenced_block()
+  local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  vim.api.nvim_buf_set_lines(0, row, row, false, { "```", "```" })
+  vim.api.nvim_win_set_cursor(0, { row + 1, 3 })
+  vim.cmd("startinsert")
+end
+
+vim.keymap.set("i", "<C-b>", insert_fenced_block, { noremap = true, silent = true })
