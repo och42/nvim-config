16737cb7236f2a4972569d30d04d46b05d0bc6bd
Jan Šidlo <1909821+och42@users.noreply.github.com>
2024-12-05 07:27:27 +0100

    Consider next line when inserting fenced block

--- lua/fenced-code-block.lua
+++ lua/fenced-code-block.lua
@@ -2,6 +2,12 @@ local function insert_fenced_block()
   local row = vim.api.nvim_win_get_cursor(0)[1]
   vim.api.nvim_buf_set_lines(0, row, row, false, { "```", "```" })
   vim.api.nvim_win_set_cursor(0, { row + 1, 3 })
+
+  local next_line = vim.api.nvim_buf_get_lines(0, row + 2, row + 3, false)[1]
+  if next_line and next_line ~= "" then
+    vim.api.nvim_buf_set_lines(0, row + 2, row + 2, false, { "" })
+  end
+
   vim.cmd("startinsert")
 end
 
