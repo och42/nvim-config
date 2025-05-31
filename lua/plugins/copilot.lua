5cd74df7940d9d200d5c277d25a4c7cba6b530bb
Jan Šidlo <1909821+och42@users.noreply.github.com>
2025-05-31 12:14:29 +0200

    Change Copilot#Accept keymap
    Default keymap <TAB> already works as regular tab in insert mode.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -20,6 +20,11 @@ return {
         desc = "Copilot suggestions",
         silent = true
       })
+      vim.keymap.set('i', '<C-Enter>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+      })
+      vim.g.copilot_no_tab_map = true
     end,
   },
   {
